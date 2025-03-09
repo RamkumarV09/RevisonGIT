729532880041a1819ea1ba200e534aefb14f2935
RamkumarV09 <ramkumarv0986@gmail.com>
2025-03-09 10:23:06 +0530

    Revise Code

---
+++ src/test/java/org/sample/SampleGIT.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class SampleGIT {
+	
+	public void meth1() {
+		
+		
+		System.out.println("Meth1");
+
+	}
+	
+	public void meth2() {
+		System.out.println("Meth 2");
+
+	}
+	
+	public void meth3() {
+
+		System.out.println("Meth3");
+	}
+
+}
